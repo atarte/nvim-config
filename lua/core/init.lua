24bf37270f14ddc92f74035c14d6054e83da42a5
atarte <tarte.antoine@gmail.com>
2023-08-06 22:01:33 +0200

    good

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -5,7 +5,8 @@ Neovim init file
 ]]--
 
 -- Import Lua core modules
+require("core.packer")
 require("core.autocmds")
 require("core.keymaps")
 require("core.options")
-require("core.packer")
+require("core.color")
--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -13,7 +13,7 @@ return require('packer').startup(function(use)
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
-
+--Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
     -- use({
     --     'rose-pine/neovim',
     --     as = 'rose-pine',
@@ -34,12 +34,14 @@ return require('packer').startup(function(use)
     --     end
     -- })
 
-    -- use {
-    --     'nvim-treesitter/nvim-treesitter',
-    --     run = function()
-    --         local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-    --         ts_update()
-    --     end, }
+    -- TreeSitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end, }
+
     -- use("nvim-treesitter/playground")
     -- use("theprimeagen/harpoon")
     -- use("theprimeagen/refactoring.nvim")
@@ -47,28 +49,35 @@ return require('packer').startup(function(use)
     -- use("tpope/vim-fugitive")
     -- use("nvim-treesitter/nvim-treesitter-context");
 
-    -- use {
-    --     'VonHeikemen/lsp-zero.nvim',
-    --     branch = 'v1.x',
-    --     requires = {
-    --         -- LSP Support
-    --         { 'neovim/nvim-lspconfig' },
-    --         { 'williamboman/mason.nvim' },
-    --         { 'williamboman/mason-lspconfig.nvim' },
+    -- LSP
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-    --         -- Autocompletion
-    --         { 'hrsh7th/nvim-cmp' },
-    --         { 'hrsh7th/cmp-buffer' },
-    --         { 'hrsh7th/cmp-path' },
-    --         { 'saadparwaiz1/cmp_luasnip' },
-    --         { 'hrsh7th/cmp-nvim-lsp' },
-    --         { 'hrsh7th/cmp-nvim-lua' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
-    --         -- Snippets
-    --         { 'L3MON4D3/LuaSnip' },
-    --         { 'rafamadriz/friendly-snippets' },
-    --     }
-    -- }
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
+
+    -- Lualine
+    use {
+        'nvim-lualine/lualine.nvim',
+        -- requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 
     -- use("folke/zen-mode.nvim")
     -- use("github/copilot.vim")
